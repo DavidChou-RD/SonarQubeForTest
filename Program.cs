eddb938f294acb7068f5f33799d4448b0828b040
DavidChou-RD <davidchou@gorilla-technology.com>
2022-11-11 14:50:30 +0800

    feat. Add C# console

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        var password = "123456";
+        if (password == "myPa55word")
+        {
+
+        }
+    }
+}
